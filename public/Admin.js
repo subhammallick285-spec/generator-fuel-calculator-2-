556c02be3fee3df49efc6eb829e70adf86174ab2
subhammallick285-spec <subhammallick285@gmail.com>
2026-09-15 21:18:35 +0530

    Update Admin.js

--- public/Admin.js
+++ public/Admin.js
@@ -1607,9 +1607,9 @@
           {
             method: "POST",
             headers: getHeaders(true),
-            body: JSON.stringify({
+                        body: JSON.stringify({
               site_id: siteId,
-              site_name: siteName,
+              date_of_filling: dateOfFilling,
               model: model,
               current_hmr: hmr,
               current_kwh: kwh,
